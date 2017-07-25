7687bef6bc00cb964e298cae3602f8e312426bee
Jorge <alcivarproanogloriahaydee@gmail.com>
2017-07-25 17:05:10 -0500

    Cambio error

--- DependsTest.php
+++ DependsTest.php
@@ -5,6 +5,10 @@ class DependsTest extends PHPUnit_Framework_TestCase
     {
         $stack = array();
         $this->assertEmpty($stack,0);
+        
+        ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
  
         return $stack;
     }
