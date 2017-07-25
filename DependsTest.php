9919fefdee0490c1f7027c9c3b7809b84f6499b2
Jorge <alcivarproanogloriahaydee@gmail.com>
2017-07-25 15:36:07 -0500

    up nn

--- DependsTest.php
+++ DependsTest.php
@@ -26,7 +26,7 @@ class DependsTest extends PHPUnit_Framework_TestCase
     public function testPop(array $stack)
     {
         $this->assertEquals('foo', array_pop($stack));
-        $this->assertEmpty($sta);
+        $this->assertEmpty($stack);
     }
 }
 ?>
